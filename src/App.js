7c4123dfe8d50626bb371604ce5533c67d18d707
crccode <54750603+crccode@users.noreply.github.com>
2023-02-24 11:30:19 -0400

    CSS Dentro del componente

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Ejemplo1 from './hooks/Ejemplo1';
 import Ejemplo2 from './hooks/Ejemplo2';
 import MiComponenteConContexto from './hooks/Ejemplo3';
 import Ejemplo4 from './hooks/Ejemplo4';
+import Greetingstyled from './components/pure/greetingStyled';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>your data</h1>
-        <TaskListComponent></TaskListComponent>
+        {/* <TaskListComponent></TaskListComponent> */}
         {/* <Ejemplo1></Ejemplo1> */}
         {/* <Ejemplo2></Ejemplo2> */}
         {/* <MiComponenteConContexto></MiComponenteConContexto> */}
@@ -20,6 +21,7 @@ function App() {
               Todo es tradato  props.children
             </h2>
         </Ejemplo4>
+        <Greetingstyled name="Marta"></Greetingstyled>
       </header>
     </div>
   );
